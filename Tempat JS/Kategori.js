580d443ad9187d3e220d5dc81eda1be13041e939
FerdinandBrian <ferdinandbrian09@gmail.com>
2025-06-08 23:40:09 +0700

    Bray 14

--- Tempat JS/Kategori.js
+++ Tempat JS/Kategori.js
@@ -3,21 +3,25 @@ fetch("../data-makanan.json")
     .then((data) => {
         const kategoriList = ["halal", "nonhalal", "jajanan"];
         kategoriList.forEach((kat) => {
-        const container = document.getElementById(kat + "-cards");
-        const items = data.filter((item) => item.kategori === kat);
-        items.forEach((item) => {
-            container.innerHTML += `
-                        <div class="col">
-                            <div class="card h-100">
-                            <img src="${item.img}" class="card-img-top" alt="${item.title}">
-                            <div class="card-body">
-                                <h5 class="card-title">${item.title}</h5>
-                                <p class="card-text">${item.desc}</p>
-                                <a href="${item.link}" class="btn btn-warning">Selengkapnya</a>
-                            </div>
-                            </div>
-                        </div>
-                        `;
-        });
+            const container = document.getElementById(kat + "-cards");
+            const items = data.filter((item) => item.kategori === kat);
+            renderCardsLoop(container, items);
         });
     });
+
+function renderCardsLoop(container, items) {
+    for (const item of items) {
+        container.innerHTML += `
+            <div class="col">
+                <div class="card h-100">
+                    <img src="${item.img}" class="card-img-top" alt="${item.title}">
+                    <div class="card-body">
+                        <h5 class="card-title">${item.title}</h5>
+                        <p class="card-text">${item.desc}</p>
+                        <a href="${item.link}" class="btn btn-warning">Selengkapnya</a>
+                    </div>
+                </div>
+            </div>
+        `;
+    }
+}
